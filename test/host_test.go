0c2455656fb25593e9699b7c10f93f386ce3bb5a
andrew brudnak <brudnak@protonmail.com>
2022-10-25 14:28:21 -0700

    💛 Rancher installed on k3s host

--- test/host_test.go
+++ test/host_test.go
@@ -1,8 +1,11 @@
 package test
 
 import (
+	"bytes"
 	"fmt"
+	"github.com/spf13/viper"
 	"os"
+	"time"
 
 	"log"
 	"testing"
@@ -12,8 +15,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestCreateHost(t *testing.T) {
+	TestHostInfrastructureCreate(t)
+	TestInstallHostRancher(t)
+}
+
 func TestHostInfrastructureCreate(t *testing.T) {
-	t.Parallel()
 
 	var dataStoreEndpoint string
 	var dataStorePassword string
@@ -48,19 +55,57 @@ func TestHostInfrastructureCreate(t *testing.T) {
 
 	token := util.RunCommand("sudo cat /var/lib/rancher/k3s/server/token", firstPublicIP)
 	serverKubeConfig := util.RunCommand("sudo cat /etc/rancher/k3s/k3s.yaml", firstPublicIP)
-	log.Println(serverKubeConfig)
+
+	log.Println("start 60 second sleep")
+	time.Sleep(60 * time.Second)
+	log.Println("end 60 second sleep")
 
 	secondServerCommand := fmt.Sprintf(`curl -sfL https://get.k3s.io | sh -s - server --token=%s --datastore-endpoint='mysql://tfadmin:%s@tcp(%s)/k3s' --tls-san %s --node-external-ip %s`, token, dataStorePassword, dataStoreEndpoint, rancherURL, secondPlublicIP)
 	var _ = util.RunCommand(secondServerCommand, secondPlublicIP)
 
+	log.Println("start 60 second sleep")
+	time.Sleep(60 * time.Second)
+	log.Println("end 60 second sleep")
+
 	actualNodeCount := util.RunCommand("sudo k3s kubectl get nodes | wc -l", firstPublicIP)
-	assert.Equal(t, "2", actualNodeCount)
+	assert.Equal(t, "3", actualNodeCount)
 
 	kubeConf := []byte(serverKubeConfig)
-	err := os.WriteFile("../config/kube_config.yml", kubeConf, 0644)
+
+	configIP := fmt.Sprintf("https://%s:6443", firstPublicIP)
+	output := bytes.Replace(kubeConf, []byte("https://127.0.0.1:6443"), []byte(configIP), -1)
+
+	err := os.WriteFile("../config/kube_config.yml", output, 0644)
 	if err != nil {
 		panic(err)
 	}
+
+	viper.AddConfigPath("../config")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yml")
+	viper.ReadInConfig()
+
+	tfvarFile := fmt.Sprintf("rancher_url = \"%s\"\nbootstrap_password = \"%s\"\nemail = \"%s\"\nrancher_version = \"%s\"", rancherURL, viper.GetString("rancher.bootstrap_password"), viper.GetString("rancher.email"), viper.GetString("rancher.version"))
+	tfvarFileBytes := []byte(tfvarFile)
+
+	err = os.WriteFile("../basetf/helm-host/terraform.tfvars", tfvarFileBytes, 0644)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("Rancher url", rancherURL)
+}
+
+func TestInstallHostRancher(t *testing.T) {
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+
+		TerraformDir: "../basetf/helm-host",
+		NoColor:      true,
+	})
+
+	terraform.InitAndApply(t, terraformOptions)
 }
 
 func TestHostCleanup(t *testing.T) {
@@ -68,5 +113,16 @@ func TestHostCleanup(t *testing.T) {
 		TerraformDir: "../basetf/hosted",
 		NoColor:      true,
 	})
+
+	err := os.Remove("../config/kube_config.yml")
+	if err != nil {
+		log.Println(err)
+	}
+
+	err = os.Remove("../basetf/helm-host/terraform.tfvars")
+	if err != nil {
+		log.Println(err)
+	}
+
 	terraform.Destroy(t, terraformOptions)
 }
